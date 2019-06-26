c7c50a168d2cccb9c3e60248bb7eabf9f923d9ec
Ashok N <nelluriashok411@gmail.com>
2019-06-26 13:42:20 +0530

    secong commit

---
+++ Provider.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name     = "Firstvpc"
+    Batch    = "7am"
+    location = "Bangalore"
+  }
+}
