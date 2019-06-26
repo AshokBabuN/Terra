2be32cbfa2670283a2be408d418897658a52437c
Ashok N <nelluriashok411@gmail.com>
2019-06-26 22:01:00 +0530

    second commit

--- Provider.tf
+++ Provider.tf
@@ -3,12 +3,24 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = "${var.vpc_cidr}"
   instance_tenancy = "default"
 
   tags = {
-    Name     = "Firstvpc"
-    Batch    = "7am"
-    location = "Bangalore"
+    Name  = "Firstvpc"
+    Batch = "7am"
+
   }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
 }
